c853f5b0af6e371ee7dfeaf474c314c78974c149
Pedro Duarte <pedrold2025@gmail.com>
2026-04-28 19:55:42 -0300

    authenticacao no animals integrado e corrigido

--- backend/animals/views.py
+++ backend/animals/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from animals.models import Animal
 from animals.serializers import AnimalSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class AnimalCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
 
 class AnimalRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
