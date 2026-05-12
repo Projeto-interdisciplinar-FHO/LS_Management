8fe0c0b9579d29519f2f3ccb728069956c7a384c
Pedro Duarte <pedrold2025@gmail.com>
2026-05-11 21:24:45 -0300

    configuracao do postgre

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -111,8 +111,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ls_management',
+        'USER': 'postgres',
+        'PASSWORD': "admin",
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
