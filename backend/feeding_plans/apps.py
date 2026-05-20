ce01b48dc0fe190754626edf571c19ce6eedaf85
HarryFelipe <harryfelipe0@gmail.com>
2026-05-20 20:56:13 -0300

    Avaliação de plano alimentar IA

--- backend/feeding_plans/apps.py
+++ backend/feeding_plans/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FeedingPlansConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'feeding_plans'
+
+    def ready(self):
+        import feeding_plans.signals 
--- backend/feeding_plans/serializers.py
+++ backend/feeding_plans/serializers.py
@@ -3,6 +3,11 @@ from feeding_plans.models import FeedingPlan
 
 
 class FeedingPlanSerializer(serializers.ModelSerializer):
+    alerta_nutricional_ia = serializers.SerializerMethodField()
+
     class Meta:
         model = FeedingPlan
         fields = '__all__'
+
+    def get_alerta_nutricional_ia(self, obj):
+        return getattr(obj, 'alerta_nutricional_ia', "Análise indisponível")
